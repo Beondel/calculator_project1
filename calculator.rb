c1f8093eed34c16233c97be3147e049eda02d07a
Beondel <bgfmac@gmail.com>
2015-07-09 16:59:42 +0000

    test

---
+++ calculator.rb
@@ -0,0 +1,19 @@
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subt(num1, num2)
+  return num1 - num2
+end
+
+def mult(num1, num2)
+  return num1 * num2
+end
+
+def div(num1, num2)
+  return num1 / num2
+end
+
+def exp(num1, num2)
+  return num1 ** num2
+end
